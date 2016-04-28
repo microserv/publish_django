b803aba74f4f2c35f7d53fd74c1502582f2d99fe
Haakon Jonsson <haakonjonsson@gmail.com>
2016-04-28 07:58:53 +0200

    django version of publish

---
+++ publish_django/urls.py
@@ -0,0 +1,28 @@
+"""publish_django URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/1.9/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.conf.urls import url, include
+    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+"""
+from django.conf.urls import url
+from django.contrib import admin
+from publish_django.views import save_article
+from publish_django.views import list
+from publish_django.views import article
+from publish_django.views import article_json
+
+urlpatterns = [
+	url(r'^save_article', save_article),
+	url(r'^list', list),
+	url(r'^article_json/*', article_json),
+	url(r'^article/*', article),
+]
---
+++ publish_django/views.py
@@ -0,0 +1,67 @@
+from django.views.decorators.csrf import csrf_exempt
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template import Context, Template
+from pymongo import MongoClient
+import requests
+import json
+from bson.objectid import ObjectId
+
+mongodb_url = "mongodb://127.0.0.1:27017/"
+indexer_url = "http://127.0.0.1:8001"
+
+client = MongoClient(mongodb_url)
+db = client["IT2901"]
+collection = db["publishing"]
+
+@csrf_exempt
+def save_article(request):
+	try:
+		r = str(collection.insert_one(request.POST.dict()).inserted_id)
+		try:
+			requests.post("indexer_url", data = {"task" : "publishedArticle" , "articleID" : r})
+		except:
+			print("Could not update indexer.")
+		return HttpResponse(status=204)
+	except:
+		return HttpResponse(status=500)
+
+@csrf_exempt
+def list(request):
+	try:
+		art_list = []
+		for art in collection.find():
+			art_list.append({"id": str(art["_id"]), "title": art["title"], "description": art["description"]})
+		return HttpResponse(json.dumps({"list":art_list}))
+	except:
+		return HttpResponse(status=500)
+
+@csrf_exempt
+def article(request):
+	try:
+		id = request.path[-24:]
+		doc = collection.find_one({'_id': ObjectId(id)})
+		return render(request, "article.html", Context({"title": doc["title"], "body": doc["article"]}));
+	except:
+		return HttpResponse(status=500)
+
+@csrf_exempt
+def article_json(request):
+	try:
+		if (request.method == "POST"):
+			id = request.path[-24:]
+			doc = collection.find_one({'_id': ObjectId(id)})
+			res = {"title": doc["title"], "description": doc["description"], "article": doc["article"], "tags": doc["tags"]}
+			return HttpResponse(json.dumps(res))
+		elif (request.method == "DELETE"):
+			id = request.path[-24:]
+			collection.delete_one({'_id': ObjectId(id)})
+			try:
+				requests.post("indexer_url", data = {"task" : "removedArticle" , "articleID" : id})
+			except:
+				print("Could not update indexer.")
+			return HttpResponse(status=204)
+		else:
+			return HttpResponse(status=500)
+	except:
+		return HttpResponse(status=500)
